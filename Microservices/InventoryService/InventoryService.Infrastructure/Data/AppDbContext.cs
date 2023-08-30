3a35b23ccda220da7f329b809539472183c67ea1
Polat Coban <polat.coban@cutek.com>
2023-08-30 16:13:01 -0500

    Autofac and Repository worked on.

--- Microservices/InventoryService/InventoryService.Infrastructure/Data/AppDbContext.cs
+++ Microservices/InventoryService/InventoryService.Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InventoryService.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryService.Infrastructure.Data;
 
---
+++ Microservices/InventoryService/InventoryService.Infrastructure/InfrastructureAutofacModule.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using Autofac;
+using InventoryService.Core.Interfaces;
+using Module = Autofac.Module;
+
+namespace InventoryService.Infrastructure;
+
+public class InfrastructureAutofacModule : Module
+{  
+    private readonly List<Assembly> _assemblies = new List<Assembly>();
+
+    public InfrastructureAutofacModule(Assembly? callingAssembly = null)
+    {
+        AddToAssembliesIfNotNull(callingAssembly);
+    }
+    private void LoadAssemblies()
+    {
+        // TODO: Replace these types with any type in the appropriate assembly/project
+        var infrastructureAssembly = Assembly.GetAssembly(typeof(InfrastructureAutofacModule));
+
+        AddToAssembliesIfNotNull(infrastructureAssembly);
+    }
+    
+    private void AddToAssembliesIfNotNull(Assembly? assembly)
+    {
+        if (assembly != null)
+        {
+            _assemblies.Add(assembly);
+        }
+    }
+    protected override void Load(ContainerBuilder builder)
+    {
+        LoadAssemblies();
+
+        RegisterEf(builder);
+
+    }
+    private void RegisterEf(ContainerBuilder builder)
+    {
+        builder.RegisterGeneric(typeof(EfRepository<>))
+            .As(typeof(IRepository<>))
+            .InstancePerLifetimeScope();
+    }
+}
