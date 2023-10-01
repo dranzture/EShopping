e2724a962cca72f408c3f9ef12c6fc0caee7e637
Polat Coban <polat.coban@cutek.com>
2023-09-30 19:39:58 -0500

    Process Payment command is fixed.

--- Microservices/Checkout/CheckoutService.Core/Commands/ProcessPaymentCommand.cs
+++ Microservices/Checkout/CheckoutService.Core/Commands/ProcessPaymentCommand.cs
@@ -1,22 +1,28 @@
 ﻿using CheckoutService.Core.Entities;
 using CheckoutService.Core.Interfaces;
-using CheckoutService.Core.ValueObjects;
 
 namespace CheckoutService.Core.Commands;
 
 public class ProcessPaymentCommand : ICommand
 {
+    private readonly CreditCard _creditCard;
+    private readonly ShoppingCart _shoppingCart;
+
     public ProcessPaymentCommand(CreditCard creditCard, ShoppingCart shoppingCart)
     {
-        
+        _creditCard = creditCard;
+        _shoppingCart = shoppingCart;
     }
     public Task<bool> CanExecute()
     {
-        throw new NotImplementedException();
+        return Task.FromResult(_creditCard.IsValidated && _shoppingCart.ShoppingItems.Sum(e => e.TotalPrice) > 0);
     }
 
     public Task Execute()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Processing payment");
+        Thread.Sleep(3000);
+        Console.WriteLine("Payment processed");
+        return Task.CompletedTask;
     }
 }
